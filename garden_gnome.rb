2c5d2f0b3bce9b535bab21353b1a4c52b5f7428c
joannajohn0214 <joannajohn0214@gmail.com>
2018-07-26 19:39:39 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -59,6 +59,8 @@ class GardenGnome
 end
 
 gnome1 = GardenGnome.new
-gnome2= GardenGnome.new.("Walter the Worst")
-gnome3= GardenGnome.new.("James the Jerk","3421")
-gnome4= GardenGnome.new.("Alfred the Abhorrent","579")
+gnome1.name =  "Walter the Worst"
+puts gnome1.name 
+gnome2= GardenGnome.new("Walter the Worst")
+gnome3= GardenGnome.new("James the Jerk","3421")
+gnome4= GardenGnome.new("Alfred the Abhorrent","579")
